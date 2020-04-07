c0376d1a0ff93ef21f2572f1118811f4440139c4
ismedina <ismaelmedinasuarez@gmail.com>
2020-04-07 23:35:03 +0200

    Added example for

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from learning_utils import *
+from learning_utils import excel_to_csv
 
 ''' This is a small script that reads each excel file in the current directory and exports it to
     a .csv file, with utf-8 encoding and the separator between columns being a tabulator.
     The output is perfect for importing it from apps such as Anki'''
-    
+
 if __name__ == "__main__":
     excel_to_csv()
